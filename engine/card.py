ec8950e8b47d0e9394928229d170a91750a73a32
tzub <tutoszub_sebastian@protonmail.com>
2026-03-20 02:39:27 -0400

    Added representation of card as integer encoding with methods to get rank and suit as well as return full deck

---
+++ engine/card.py
@@ -0,0 +1,13 @@
+# Integer encoding for cards: rank * 4 + suit
+
+card_ranks = [2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
+card_suit = ["SPADE", "CLUB", "HEART", "DIAMOND"]
+
+def rank_of(card):
+    return card // 4
+
+def suit_of(card):
+    return card % 4 
+
+def make_deck():
+    return list(range(52))
