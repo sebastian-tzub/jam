912d881eda0105aca9a51af0ac2af0768e9fb6a3
tzub <tutoszub_sebastian@protonmail.com>
2026-03-23 12:39:01 -0400

    Added hand equity calculation for the turn

--- engine/equity.py
+++ engine/equity.py
@@ -1,4 +1,5 @@
 from hand_evaluator import best_of_seven_cards
+import numpy as np
 
 # ASSUMES EXACT HAND OF PLAYERS IS KNOWN (specific hand not range)
 # Returns list of equities for each player 
@@ -12,16 +13,16 @@ def hand_equity(hands, board):
     length = len(board)
 
     if length == 3: 
-        return hand_equity_flop(hand, board, remaining)
+        return hand_equity_flop(hands, board)
     elif length == 4: 
-        return hand_equity_turn(hand, board, remaining)
+        return hand_equity_turn(hands, board, remaining)
     elif length == 5: 
-        return hand_equity_river(hand, board, remaining)
+        return hand_equity_river(hands, board, remaining)
     else: 
         return hand_equity_preflop()
 
 
-def hand_equity_river(hand, board, remaining): 
+def hand_equity_river(hands, board): 
 
     hand_tuples = [] #list of tuples for each hand returned by hand_evaluator methods 
 
@@ -52,14 +53,27 @@ def hand_equity_river(hand, board, remaining):
 # Evaluate each players hand with complete board 
 # Track wins across each runout 
 # Dive by wins by total runouts to get equity 
-def hand_equity_turn(hand, board, remaining): 
+def hand_equity_turn(hands, board, remaining): 
+
+    river_runouts = combinations(remaining, 1)
+    total_equities = np.zeros(len(hands)) # tracks equities for each player across all possible runouts 
+    num_runouts = 0
 
     # for each possible run out calculate 
+    for river_card in river_runouts: 
+        complete_board = board + list(river_card)
+        curr_equities = hand_equity_river(hand, complete_board)
+        total_equities = [i + j for i, j in zip(curr_equities, total_equities)]
+        num_runouts += 1
 
     # divide wins for each player by total runouts to get equity 
+    player_equities = [] # ACTUAL equities of each player (total_equities / num of runouts)
+    for i, equity in enumerate(total_equities): 
+        player_equities.append(total_equities[i] / num_runouts)
 
-    return equities 
-def hand_equity_flop(hand, board, remaining): 
+    return player_equities 
+
+def hand_equity_flop(hands, board, remaining): 
 
     return equities 
 
